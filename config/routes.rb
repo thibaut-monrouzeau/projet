ee6bd4b8b68f8531cde33220f8b771d85369d5d5
thibaut.monrouzeau <thibaut.monrouzeau@outlook.fr>
2017-03-22 09:43:14 -0400

    page_accueil

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :students
-  
- map.root :controller => "students"
+ 
+  get 'students'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
