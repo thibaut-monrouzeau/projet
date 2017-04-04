2aa7c8b68934162241ea1635e65c5849a6fbee37
thibaut.monrouzeau <thibaut.monrouzeau@outlook.fr>
2017-04-04 09:13:15 -0400

    meteo

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -9,7 +9,6 @@ class LocationsController < ApplicationController
 
   # GET /locations/1
   # GET /locations/1.json
-  def show 
   def show
      forecast = ForecastIO.forecast(@locations.latitude, @locations.longitud)
      weatherOk = false
